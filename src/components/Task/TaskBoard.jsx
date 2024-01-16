6520e5a317a8b93b72422e34e0abf5b4c2b8957b
asus test <msa-99@outlook.com>
2024-01-16 18:11:21 +0600

    add and edit

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -13,17 +14,49 @@ const TaskBoard = () => {
         'tags':['React', 'JS', 'Web']
     }
     const [tasks, setTasks]=useState([defaultTask])
+    const [showAddTaskModal, setShowAddTaskModal]=useState(false)
+    const [taskToUpdate, setTaskToUpdate]=useState(null)
+
+    //add korbe and update korbe
+    const handleAddTask=(newTask,e,isAdd)=>{
+        e.preventDefault()
+        if(isAdd){
+            setTasks([
+                ...tasks,newTask
+                 ])
+        }
+        else{
+            setTasks(tasks.map(task=>{
+                if(task.id===newTask.id){
+                    return newTask
+                }
+                return task
+             }))
+        }
+    
+       setShowAddTaskModal(false)
+    }
+
+    const handleEditTask=(taskToEdit)=>{
+       setTaskToUpdate(taskToEdit)
+       setShowAddTaskModal(true)
+
+       
+    }
+    
     return (
         <section className="mb-20" id="tasks">
-		
+		{showAddTaskModal&&<AddTaskModal 
+        onSave={handleAddTask}
+        taskToUpdate={taskToUpdate} />}
 		<div className="container">
 		<div className="p-2 flex justify-end">
 			<SearchTask/>
 		</div>
 			<div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-				<TaskActions/>
+				<TaskActions onAddTaskClick={()=>setShowAddTaskModal(true)}/>
 				<div className="overflow-auto">
-					<TaskList tasks={tasks}/>
+					<TaskList onEdit={handleEditTask} tasks={tasks}/>
 				</div>
 			</div>
 		</div>
--- src/components/Task/TaskList.jsx
+++ src/components/Task/TaskList.jsx
@@ -1,5 +1,5 @@
 import { FaStar } from 'react-icons/fa';
-const TaskList = ({tasks}) => {
+const TaskList = ({tasks, onEdit}) => {
     // console.log(tasks);
     
     return (
@@ -40,7 +40,8 @@ const TaskList = ({tasks}) => {
                 <td>
                     <div className="flex items-center justify-center space-x-3">
                         <button className="text-red-500">Delete</button>
-                        <button className="text-blue-500">Edit</button>
+                        <button onClick={()=>onEdit(task)}
+                         className="text-blue-500">Edit</button>
                     </div>
                 </td>
             </tr>
